9232685829bf832425f5112da4db28231f9abc77
sntwandy <sntwandy@gmail.com>
2020-07-05 15:55:12 -0400

    Understanding Mono hilo in NodeJS

---
+++ concepts/monohilo.js
@@ -0,0 +1,12 @@
+console.log('Hello Megan E.');
+
+let i = 0;
+setInterval(function () {
+    console.log(i);
+    i++;
+    /* if (i === 5) {
+        let a = 3 + z;
+    };*/
+
+}, 1000);
+console.log('Second instruction');
