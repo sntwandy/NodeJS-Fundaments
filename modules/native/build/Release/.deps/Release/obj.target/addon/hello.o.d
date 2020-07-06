6298dbf5e6b729b52b05dc5bcba17b188366cc6b
sntwandy <sntwandy@gmail.com>
2020-07-05 23:26:26 -0400

    Executing Cpp modules in NodeJS

---
+++ modules/native/build/Release/.deps/Release/obj.target/addon/hello.o.d
@@ -0,0 +1,17 @@
+cmd_Release/obj.target/addon/hello.o := g++ -o Release/obj.target/addon/hello.o ../hello.cc '-DNODE_GYP_MODULE_NAME=addon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D__STDC_FORMAT_MACROS' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DBUILDING_NODE_EXTENSION' -I/home/sntwady/.cache/node-gyp/14.5.0/include/node -I/home/sntwady/.cache/node-gyp/14.5.0/src -I/home/sntwady/.cache/node-gyp/14.5.0/deps/openssl/config -I/home/sntwady/.cache/node-gyp/14.5.0/deps/openssl/openssl/include -I/home/sntwady/.cache/node-gyp/14.5.0/deps/uv/include -I/home/sntwady/.cache/node-gyp/14.5.0/deps/zlib -I/home/sntwady/.cache/node-gyp/14.5.0/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/addon/hello.o.d.raw   -c
+Release/obj.target/addon/hello.o: ../hello.cc \
+ /home/sntwady/.cache/node-gyp/14.5.0/include/node/node.h \
+ /home/sntwady/.cache/node-gyp/14.5.0/include/node/v8.h \
+ /home/sntwady/.cache/node-gyp/14.5.0/include/node/v8-internal.h \
+ /home/sntwady/.cache/node-gyp/14.5.0/include/node/v8-version.h \
+ /home/sntwady/.cache/node-gyp/14.5.0/include/node/v8config.h \
+ /home/sntwady/.cache/node-gyp/14.5.0/include/node/v8-platform.h \
+ /home/sntwady/.cache/node-gyp/14.5.0/include/node/node_version.h
+../hello.cc:
+/home/sntwady/.cache/node-gyp/14.5.0/include/node/node.h:
+/home/sntwady/.cache/node-gyp/14.5.0/include/node/v8.h:
+/home/sntwady/.cache/node-gyp/14.5.0/include/node/v8-internal.h:
+/home/sntwady/.cache/node-gyp/14.5.0/include/node/v8-version.h:
+/home/sntwady/.cache/node-gyp/14.5.0/include/node/v8config.h:
+/home/sntwady/.cache/node-gyp/14.5.0/include/node/v8-platform.h:
+/home/sntwady/.cache/node-gyp/14.5.0/include/node/node_version.h:
